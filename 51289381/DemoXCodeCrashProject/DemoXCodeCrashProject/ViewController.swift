4fd462c2651eec09369a14ec9a91f95274276df3
deemicksee <daniel.mcclelland@outlook.com>
2019-05-31 16:33:45 +1200

    51289381: Sample project produces an XCode crash or at least an extreme delay if you uncomment the file UncommentThisFileToCrashXCode.swift while XCode is open

---
+++ 51289381/DemoXCodeCrashProject/DemoXCodeCrashProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoXCodeCrashProject
+//
+//  Created by dmcclelland on 31/05/19.
+//  Copyright © 2019 dmcclelland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
